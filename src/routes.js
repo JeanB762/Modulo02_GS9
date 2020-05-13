f6c005950af3503e1988da949102e3417dfc60e3
jeanb762 <jeanborges946@gmail.com>
2020-05-13 16:33:17 -0300

    Configuraçao password_hash

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,9 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
 import User from './app/models/Users';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Jean Borges',
-    email: 'jean@gmail.com',
-    password_hash: '1231456489',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
