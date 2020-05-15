7c226e375707e6d98b69e6af7b7ff00d2093e060
jeanb762 <jeanborges946@gmail.com>
2020-05-14 22:24:07 -0300

    feature: authentication middlewares

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // Define o uso do Middleware para todas as rotas abaixo
+routes.put('/users', UserController.update);
 
 export default routes;
